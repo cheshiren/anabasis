2923f99eaabd93d647a4f6aecb53716e66dd4fde
cheshiren <vd.cheshire@gmail.com>
2015-02-10 13:52:14 +0500

    Delete compile.bat

--- compile.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Dropbox\!IF\!!Flaming\IF3D"
-"C:\Python27\python.exe" "C:\Dropbox\!IF\Platforms\twee-1.7.1\twine-master\twee" -t sugarcube IF3Dche.twee Scripts.twee CSS.twee > IF3Dche.html
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" IF3Dche.html
